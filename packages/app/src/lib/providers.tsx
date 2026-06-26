774c271a09e28effbf409c81a0430a524f21ee96
rinat <ribroff@gmail.com>
2026-06-26 13:13:17 -0600

    public api

--- packages/app/src/lib/providers.tsx
+++ packages/app/src/lib/providers.tsx
@@ -11,7 +11,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3001/trpc",
+          url: `${process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001"}/trpc`,
         }),
       ],
     })
