c329f89118d164e8bc06cdb8adf988068d59aff1
rinat <ribroff@gmail.com>
2026-06-26 13:18:31 -0600

    cross origin policy setup

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,7 +5,7 @@ import { appRouter } from "./router.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN ?? "http://localhost:3000" }));
 app.use(express.json());
 
 app.use(
